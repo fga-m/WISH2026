27665e665a872f80528998e03355ed0ccf48fd2e
fga-m <media@fgam.org.au>
2026-03-17 14:27:14 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// This script finds the 'root' div in your index.html and injects the React app into it.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
